91efb40b678480152083dba9a0a71b1e8f191ffa
shivalib1 <shivali.bhanpurkar@gmail.com>
2018-03-30 14:27:13 +0530

    ui

--- app/src/main/java/com/example/anany/vnit_connect/ForumActivity.java
+++ app/src/main/java/com/example/anany/vnit_connect/ForumActivity.java
@@ -14,23 +14,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class ForumActivity extends AppCompatActivity {
@@ -39,11 +31,9 @@ public class ForumActivity extends AppCompatActivity {
     private Ques_descAdapter mAdapter;
     private ArrayList<Ques_desc> questionList = new ArrayList<>();
     private RecyclerView recyclerView;
-    private Question ques;
     private String uid, email, name;
     private FirebaseFirestore db;
     private FirebaseUser user;
-    //private ArrayList<User> mArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,13 +52,9 @@ public class ForumActivity extends AppCompatActivity {
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
         recyclerView.setAdapter(mAdapter);
 
+        //Database connection
         db = FirebaseFirestore.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
-
-        if (user != null) {
-            email = user.getEmail();
-            uid = user.getUid();
-        }
 
         /*Query query = db.collection("user").whereEqualTo("email", email);
         query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
@@ -88,63 +74,47 @@ public class ForumActivity extends AppCompatActivity {
             }
         });*/
 
-        ArrayList<String> interests = new ArrayList<String>();
-        interests.add("A");
-        interests.add("B");
-        interests.add("D");
-        final User user = new User(name, email, interests);
-
-        final Map<String, Object> mapuser = new HashMap<>();
-        mapuser.put("email", email);
-        mapuser.put("interests",interests);
-
-        DocumentReference docRef = db.collection("user").document("iupAgvor1ptsvTz5ahrp");
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document != null && document.exists()) {
-                        User u = document.toObject(User.class);
-                        name = u.getUsername();
-                        user.setUsername(name);
-                        mapuser.put("username", name);
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        System.out.println(user.getUsername());
-
-                        // Add a new document with a generated ID
-                        db.collection("user").document("map_user1")
-                                .set(mapuser)
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Log.d(TAG, user.getUsername());
-                                        Log.d(TAG, "DocumentSnapshot successfully written!");
-                                    }
-                                })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.w(TAG, "Error writing document", e);
-                                    }
-                                });
-
-                    }
-                    else {
-                        Log.d(TAG, "No such document");
-                    }
-                }
-                else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
-            }
-        });
-
+        addUserQuestion();
         getListItems();
 
     }
 
-    public void getListItems() {
+    private void addUserQuestion() {
+        //Get user details
+        if (user != null) {
+            email = user.getEmail();
+            uid = user.getUid();
+            name = user.getDisplayName();
+        }
+
+        //Add interests in ArrayList
+        ArrayList<String> interests = new ArrayList<String>();
+        interests.add("A");
+        interests.add("B");
+        interests.add("D");
+
+        //Create user object
+        User user_obj = new User(name, email, interests);
+
+        // Add a new document with a given ID
+        db.collection("user").document(uid)
+                .set(user_obj)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
+    }
+
+    private void getListItems() {
         db.collection("ques_desc").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
@@ -154,14 +124,14 @@ public class ForumActivity extends AppCompatActivity {
                             return;
                         }
                         else {
-                            // Convert the whole Query Snapshot to a list
-                            // of objects directly! No need to fetch each
-                            // document.
+                            // Convert the whole Query Snapshot to a list of objects directly
+                            // No need to fetch each document.
                             List<Ques_desc> types = documentSnapshots.toObjects(Ques_desc.class);
 
                             // Add all to your list
                             questionList.addAll(types);
                             Log.d(TAG, "onSuccess: Yay! " + questionList);
+                            mAdapter.notifyDataSetChanged();
                         }
                     }
                 })
@@ -171,6 +141,7 @@ public class ForumActivity extends AppCompatActivity {
                         Toast.makeText(getApplicationContext(), "Error getting data!!!", Toast.LENGTH_LONG).show();
                     }
                 });
+
     };
 
     @Override
